d38ae8e1dc0aa427ebb46b64f44d1f711fcc05ca
Virat Poddar <viratpoddar2012@gmail.com>
2026-04-14 17:13:18 +0530

    pathname customized

--- components/wrapper/copilot.js
+++ components/wrapper/copilot.js
@@ -2,15 +2,19 @@
 
 import { CopilotKit } from "@copilotkit/react-core";
 import { CopilotPopup } from "@copilotkit/react-ui";
-import { useSession } from "next-auth/react";
 import "@copilotkit/react-ui/styles.css";
+import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation"
 
 export default function Copilot({ children }) {
   const { data: session } = useSession();
+  const pathname = usePathname();
+  const inActive = pathname === "/home"
+
   return (
     <CopilotKit runtimeUrl={`${process.env.NEXT_PUBLIC_SERVER_URL}/ai/copilot`}>
       {children}
-      {session && (
+      {session !inActive && (
         <div className="fixed bottom-4 right-4 z-[9999]">
           <CopilotPopup
             instructions="You are a helpful AI assistant."
