7637b5dd10cde389fe4434aeefee997f0fc75730
Virat Poddar <viratpoddar2012@gmail.com>
2026-05-01 22:46:10 +0530

    Update changes to show assistant on every page..

--- components/wrapper/copilot.js
+++ components/wrapper/copilot.js
@@ -3,18 +3,16 @@
 import { CopilotKit } from "@copilotkit/react-core";
 import { CopilotPopup } from "@copilotkit/react-ui";
 import "@copilotkit/react-ui/styles.css";
-import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation"
 
 export default function Copilot({ children }) {
-  const { data: session } = useSession();
   const pathname = usePathname();
   const inActive = pathname === "/home"
 
   return (
     <CopilotKit runtimeUrl={`${process.env.NEXT_PUBLIC_SERVER_URL}/ai/copilot`}>
       {children}
-      {session && !inActive && (
+      {!inActive && (
         <CopilotPopup
   className="!fixed !bottom-16 sm:!bottom-4 !right-4 !z-[9999]"
   instructions="You are a helpful AI assistant."
